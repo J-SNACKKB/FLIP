08f453f83497fa90af570bf5ac0ec4f9539e02a3
jmou2 <jodymou@gmail.com>
2022-01-14 14:23:26 -0500

    get rid of bash trainfiles before main merge

--- baselines/train_linear.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for dataset in aav_1 aav_2 aav_3 aav_4 aav_5 aav_5 aav_6 aav_7 meltome_mixed meltome_human meltome_humancell meltome_humancell gb1_1 gb1_2 gb1_3 gb1_4 gb1_5 
-do 
-    python train_all.py $dataset ridge --gb1_shorten
-done 
-
