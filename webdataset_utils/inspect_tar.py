1061be6bd3630e2d7eb7da785391d9515669c480
Maya <mayaczeneszew@gmail.com>
2024-08-20 14:32:48 +0200

    Remove webdataset_utils from helpers-cnn branch

--- webdataset_utils/inspect_tar.py
+++
@@ -1,9 +0,0 @@
-import tarfile
-
-def inspect_tar(tar_file):
-    with tarfile.open(tar_file, "r") as tar:
-        for member in tar.getmembers():
-            print("Member:", member.name)
-            f = tar.extractfile(member)
-            content = f.read() if f else None
-            print("Content:", content)
